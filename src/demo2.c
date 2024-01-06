5ebb429c9b952ee8fb8b37d679f3c4e3dbf3af01
Brett Calhoun <lbrettcalhoun@gmail.com>
2024-01-05 20:34:33 -0500

    Initial commit

---
+++ src/demo2.c
@@ -0,0 +1,16 @@
+#define __AVR_ATtiny85__
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void) {
+    DDRB = (1<<PB4);
+    PORTB = (1<<PB4);
+
+    while (1) {
+        _delay_ms(500);
+        PORTB = (0<<PB4);
+        _delay_ms(500);
+        PORTB = (1<<PB4);
+    }
+    return 0;
+}
