439c92cfe0d45ba63a6e75d3b0daf0a04f80ae41
Brett Calhoun <lbrettcalhoun@gmail.com>
2024-09-30 18:39:43 -0400

    Fix: Add conditional define and F_CPU to adcnr.c

--- src/adcnr.c
+++ src/adcnr.c
@@ -3,9 +3,13 @@
     ADC Noise Reduction: 1 of the 3 basic types of low power mode for ATtiny85
 */
 
-#define __AVR_ATtiny85__
+#ifndef __AVR_ATtiny85__
+  #define __AVR_ATtiny85__
+#endif
+#define F_CPU 8000000
 #include <avr/io.h>
 #include <util/delay.h>
+
 
 int main (void) {
     // Setup pin 4 as output and set pin 4 high
@@ -16,18 +20,18 @@ int main (void) {
     // Toggle pin 4 (10x). Actual wall time for delay will be approximately 8 secs when using no clock prescale (1)
     // Current draw will vary from 6.7 mA - 8.2 mA
     while (i <= 10) {
-        _delay_ms(16000);
+        _delay_ms(8000);
         PORTB &= ~(1<<PB4);
-        _delay_ms(16000);
+        _delay_ms(8000);
         PORTB |= (1<<PB4);
         i++;
     }
     // Pause for approximately 30 secs ... 8.2 mA
-    _delay_ms(64000);
+    _delay_ms(30000);
     // Set pin 4 low
     PORTB &= ~(1<<PB4);
     // Pause another 30 secs ... 6.7 mA
-    _delay_ms(64000);
+    _delay_ms(30000);
 
     // Enable ADC noise reduction low power mode
     // Set the sleep enable bit and the ADC noise reduction sleep mode bit
